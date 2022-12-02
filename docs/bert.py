[TorchMemModel] Fix gradient and weight tensor annotation.

docs/bert.py
@@ -12,7 +12,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import copy
-from ratex.utils.mem_model_utils import analyze_training_peak_memory, profile_training_peak_memory, print_mem_breakdown, wrap_model
+from ratex.utils.mem_model_utils import analyze_training_memory, profile_training_peak_memory, print_mem_breakdown, wrap_model
 from transformers import BertForSequenceClassification, BertConfig
 from accelerate import init_empty_weights
 import time
@@ -57,7 +57,7 @@ def main():
     model_lt = wrap_model(model_lt, 'top', max_depth=4)
     model_lt = model_lt.to(device="lazy")
     loss_fn = nn.CrossEntropyLoss()
-    peak_memory_ltc, mem_breakdown, node_info = analyze_training_peak_memory(
+    peak_memory_ltc, mem_breakdown, node_info = analyze_training_memory(
         model_lt, loss_fn, 
         input_shape=(bs, seq_len), output_shape=(bs,), 
         input_dtype=torch.int64, output_dtype=torch.int64, 

docs/lenet2.py
@@ -20,7 +20,7 @@ from torchvision import datasets, models, transforms
 import time
 import os
 import copy
-from ratex.utils.mem_model_utils import analyze_training_peak_memory, print_mem_breakdown, profile_training_peak_memory, wrap_model
+from ratex.utils.mem_model_utils import analyze_training_memory, print_mem_breakdown, profile_training_peak_memory, wrap_model
 import accelerate
 import json
 
@@ -55,7 +55,7 @@ def main():
     model_lt = wrap_model(model_lt, 'top')
     model_lt = model_lt.to(device="lazy", dtype=torch.float32)
     loss_fn = torch.nn.NLLLoss()
-    peak_memory_ltc, mem_breakdown, ir_info = analyze_training_peak_memory(
+    peak_memory_ltc, mem_breakdown, ir_info = analyze_training_memory(
         model_lt, loss_fn, (4, 3, 32, 32), (4,), torch.float32, torch.int64, output_range=[0, 10])
     
     model_cuda = TorchLeNet()

ratex/utils/mem_model_utils.py
@@ -119,12 +119,13 @@ def print_mem_breakdown(mem_breakdown):
             print('  |-bwd: peak {0:6.4f}, param {1:6.4f}, input_act {2:6.4f}, output_act {3:6.4f}, isolated {4:6.4f}'.format(
                 info['bwd']['peak_mem'], info['bwd']['param'], info['bwd']['input_act'], info['bwd']['output_act'], info['bwd']['peak_mem_isolated']))
 
-def analyze_training_peak_memory(model, loss_fn, input_shape, output_shape, 
-                                 input_dtype, output_dtype, input_range=None, output_range=None, 
-                                 n_batches=2):
+def analyze_training_memory(model, loss_fn, input_shape, output_shape, 
+                            input_dtype, output_dtype, input_range=None, output_range=None, 
+                            n_batches=2):
     """
-    Get the peak memory consumption while training the model using an analysis
-    pass on the lazy tensor IR. Assuming that the model is in device "lazy". 
+    Get the memory consumption information while training the model using an analysis
+    pass on the lazy tensor IR. Assuming that the model is in device "lazy". No optimizer
+    is used when analyzing memory. 
 
     Args:
         model: torch.nn.Module  The model to be analyzed. 
@@ -138,6 +139,9 @@ def analyze_training_peak_memory(model, loss_fn, input_shape, output_shape,
     
     Returns:
         peak_mem_mbs: Peak memory consumption in MBs. 
+        breakdown_dict: A dictionary containing the memory breakdown per layer. The granularity is
+                        determined when the model is wrapped. 
+        ir_info: A JSON containing detailed information about each IR node in LTC IR. 
     """
 
     start = time.time() 
@@ -188,7 +192,8 @@ def analyze_training_peak_memory(model, loss_fn, input_shape, output_shape,
 
     end = time.time()
     print('Memory model elapsed time: ', end - start)
-    return peak_mem_mbs, breakdown_dict, lm.get_ltc_ir_info()
+    ir_info = lm.get_ltc_ir_info()
+    return peak_mem_mbs, breakdown_dict, ir_info
 
 def profile_training_peak_memory(model, loss_fn, input_shape, output_shape, 
                                  input_dtype, output_dtype, input_range=None, output_range=None, n_batches=2):